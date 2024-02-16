255c0754551f4576e5fe3a7c91bd3c73cced790b
Venkatesan Ramalingam <venkatr@stengg.com>
2024-02-16 21:27:36 +0800

    variables

---
+++ 002-variables/prime-variables.py
@@ -0,0 +1,31 @@
+
+# Variables
+
+a1 = 1
+b1 = 2
+print(a1)
+print(b1)
+
+a2 = 3       # x is of type int
+b2 = "Sally" # x is now of type str
+print(a2)
+print(b2)
+
+a3 = str(3)    # x will be '3'
+b3 = int(3)    # y will be 3
+c3 = float(3)  # z will be 3.0
+print(a3)
+print(b3)
+print(c3)
+
+a4 = 5
+b4 = "John"
+print(type(a4))
+print(type(b4))
+
+
+a5= "John"
+# is the same as
+b5 = 'John'
+print(a5 == b5)
+
